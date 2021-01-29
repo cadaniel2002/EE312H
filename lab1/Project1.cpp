92eb7ba054e5460a3998c0ae2ed22ad4e473f0bd
cadaniel2002 <cadaniel.2002@gmail.com>
2021-01-29 17:24:42 -0600

    first commit

---
+++ lab1/Project1.cpp
@@ -0,0 +1,21 @@
+/*
+ * Project1.cpp
+ *
+ * Name: Ima Student
+ * EE312 Summer 2020
+ * SuperStrings
+ */
+
+#include <stdio.h> // provides declarations for printf and putchar
+#include <stdint.h> // provides declarations for int32_t uint32_t and the other (new) standard C types
+
+/*
+All of your code must be in this file. Please no #includes other than standard system headers (ie.., stdio.h, stdint.h)
+
+You must write this function (printSuperStrings). Do not change the way the function is declared (i.e., it has
+exactly two parameters, each parameter is a standard (mundane) C string (see Instruction).
+*/
+
+void printSuperStrings(char targets [], char candidates []) {
+
+}
